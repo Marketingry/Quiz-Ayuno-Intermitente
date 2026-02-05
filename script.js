e9d380e9f87e4d715ac62b81cfcba725b3c2c87a
Ryan Nascimento <opmarketingvz@gmail.com>
2026-02-04 21:48:59 -0300

    perf: reduce step transition delay and preload critical assets

--- script.js
+++ script.js
@@ -506,7 +506,7 @@ function handleOptionSelect(card) {
         if (!step.classList.contains('interlude')) {
             setTimeout(() => {
                 goToNextStep();
-            }, 300);
+            }, 70);
         }
     }
 }
